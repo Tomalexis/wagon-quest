efdf03be17777caea6ae6685bf6542dbfb44aa88
Paryssatis Khazaie <paryssatis@outlook.com>
2022-09-05 17:14:05 +0200

    commented out console logs

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -13,36 +13,36 @@ export default class extends Controller {
   }
 
   connect() {
-    console.log('connected')
-    console.log(this.userTarget)
-    console.log(this.userPositionXValue)
-    console.log(this.userPositionYValue)
-    console.log(this.obstacleTargets)
+    // console.log('connected')
+    // console.log(this.userTarget)
+    // console.log(this.userPositionXValue)
+    // console.log(this.userPositionYValue)
+    // console.log(this.obstacleTargets)
 
     window.addEventListener('keydown', this.logKey.bind(this));
   }
   logKey(e) {
-    console.log(e.code)
+    // console.log(e.code)
     let newPositionX = this.userPositionXValue
     let newPositionY = this.userPositionYValue
 
     if (e.code == "ArrowLeft") {
-      console.log('move to the left')
+      // console.log('move to the left')
       if (this.userPositionXValue > this.mapPositionXMinValue){
         newPositionX = this.userPositionXValue - 1
       }
     } else if (e.code == "ArrowRight") {
-      console.log('move to the right')
+      // console.log('move to the right')
       if (this.userPositionXValue < this.mapPositionXMaxValue){
         newPositionX = this.userPositionXValue + 1
       }
     } else if (e.code == "ArrowUp") {
-      console.log('move up')
+      // console.log('move up')
       if (this.userPositionYValue > this.mapPositionYMinValue){
         newPositionY = this.userPositionYValue - 1
       }
     } else if (e.code == "ArrowDown") {
-      console.log('move down')
+      // console.log('move down')
       if (this.userPositionYValue < this.mapPositionYMaxValue){
         newPositionY = this.userPositionYValue + 1
       }
