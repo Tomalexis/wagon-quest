654db9a285dacf6d6516a4fe179d64588dc85886
Luther Tchofo-Safo <nintendobserver@gmail.com>
2022-09-06 15:16:34 +0200

    Le son fonctionne.

--- app/javascript/controllers/game_controller.js
+++ app/javascript/controllers/game_controller.js
@@ -2,12 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="music-in-battle"
 export default class extends Controller {
-  static targets = ["continueLink"]
+  static targets = ["continueLink", "answerLink"]
 
   connect() {
     this.csrf = document.querySelector('[name="csrf-token"]').content
     console.log("Is this thing on?")
-    console.log(this.continueLinkTarget)
+    // console.log(this.continueLinkTarget)
     // console.log(this.continueLinkTarget.href)
   }
 
@@ -16,6 +16,21 @@ export default class extends Controller {
     console.log("Continue?")
     fetch(this.continueLinkTarget.href, {
       method: "PATCH",
+      headers: { "Accept": "application/json", "X-CSRF-TOKEN": this.csrf }
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data)
+        this.element.outerHTML = data.battle
+      })
+  }
+
+  answer(event) {
+    event.preventDefault()
+    console.log("Continue?")
+    const answerClickedLink = this.answerLinkTargets.find(link => link == event.currentTarget);
+    fetch(answerClickedLink.href, {
+      method: "POST",
       headers: { "Accept": "application/json", "X-CSRF-TOKEN": this.csrf }
     })
       .then(response => response.json())
