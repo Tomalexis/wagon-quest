e1dfdde0f09c15cebddbfc54e8d959f98cf88585
Thomas Girard <thomasalexisgirard@gmail.com>
2022-09-08 11:25:26 +0200

    bind space to continue in battles

--- app/javascript/controllers/game_controller.js
+++ app/javascript/controllers/game_controller.js
@@ -3,16 +3,40 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="music-in-battle"
 export default class extends Controller {
   static targets = ["continueLink", "answerLink", "form"]
+  static values = {battleStatus: String}
 
   connect() {
     this.csrf = document.querySelector('[name="csrf-token"]').content
     // console.log("Is this thing on?")
+    // console.log(this.battleStatusValue)
     // console.log(this.continueLinkTarget)
     // console.log(this.continueLinkTarget.href)
+
+    window.addEventListener('keydown', this.logKey.bind(this));
+  }
+
+  logKey(e) {
+    // console.log(e.code)
+
+    // console.log(this.element.hidden)
+
+    if (e.code == "Space") {
+      // console.log('continue')
+      // console.log(this.continueTargets[this.currentDialogValue - 1])
+      if(this.battleStatusValue == 'battle_outro') {
+        this.continueLinkTarget.click()
+      } else if(this.battleStatusValue != 'round_core') {
+        const event = new Event('click');
+
+        this.continueLinkTarget.dispatchEvent(event);
+      }
+
+    }
   }
 
   continue(event) {
     event.preventDefault()
+
     // console.log("Continue?")
     fetch(this.continueLinkTarget.href, {
       method: "PATCH",
