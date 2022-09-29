35e05196f9e735fb7cd509f15cd4f3341ce1e942
Luther Tchofo-Safo <nintendobserver@gmail.com>
2022-09-30 01:04:41 +0200

    Allowed game.teacher via game model, through: :battles.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ApplicationRecord
   belongs_to :user
   has_many :battles
+  has_many :teachers, through: :battles
   has_many :game_answers
   validates :status, inclusion: { in: %w[intro battle map clear] }
 end
