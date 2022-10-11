657a8082fcc19e693700af2f019e0ccd2ac7edf0
Luther Tchofo-Safo <nintendobserver@gmail.com>
2022-10-11 22:26:58 +0200

    At least it doesn't freeze now.

--- app/javascript/controllers/clear_animation_controller.js
+++ app/javascript/controllers/clear_animation_controller.js
@@ -4,17 +4,15 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   connect() {
-  }
-}
 
-const canvas = document.getElementById('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-let ctx = canvas.getContext('2d');
+    const canvas = document.getElementById('canvas');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let ctx = canvas.getContext('2d');
 
-class Particle{
-    constructor(y){
-       this.x = (Math.random() * canvas.width);
+    class Particle{
+      constructor(y){
+        this.x = (Math.random() * canvas.width);
        this.y = y;
        this.size = (Math.floor(Math.random() * 10)) + 10;
        this.hue = (Math.floor(Math.random() * 360));
@@ -22,40 +20,43 @@ class Particle{
        this.degrees = Math.random() < 0.5 ? 0.1 : -0.1;
        this.angle = 0;
        this.rate = (Math.floor(Math.random() * 2)) + 0.5;
-    }
-    draw(){
+      }
+      draw(){
         ctx.fillStyle = `hsl(${this.hue},50%,50%)`;
         ctx.save();
         ctx.translate(this.x,this.y);
         ctx.rotate(this.angle);
         ctx.fillRect(0 - this.size / 2,0 - this.size / 2,this.size,this.size);
         ctx.restore();
-    }
-    update(){
+      }
+      update(){
         if(this.y > canvas.height){
-            particleArray.push(new Particle(-50));
-            this.gone = true;
+          particleArray.push(new Particle(-50));
+          this.gone = true;
         }
         this.y += this.rate;
         this.angle += this.degrees;
         this.draw();
+      }
     }
-}
 
-let particleArray = [];
+    let particleArray = [];
 
-for(var i = 0; i < 400; i++){
-    particleArray.push(new Particle(
+    for(var i = 0; i < 400; i++){
+      particleArray.push(new Particle(
         Math.random() * canvas.height
-    ));
-}
+        ));
+      }
 
-function animate(){
-    requestAnimationFrame(animate);
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    for(var i = 0; i < particleArray.length; i++){
-        particleArray[i].update();
+      function animate(){
+        requestAnimationFrame(animate);
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        for(var i = 0; i < particleArray.length; i++){
+          particleArray[i].update();
+        }
+        particleArray = particleArray.filter(particle => !particle.gone);
+      }
+      animate();
+
     }
-    particleArray = particleArray.filter(particle => !particle.gone);
-}
-animate();
+  }
